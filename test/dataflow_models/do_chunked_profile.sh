030585c831129778571bd70475cb0d9e0896b0b6
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2025-05-02 14:28:50 -0400

    Adding profiling script with h100 as default device

--- test/dataflow_models/do_chunked_profile.sh
+++ test/dataflow_models/do_chunked_profile.sh
@@ -1,1 +1,1 @@
-nsys profile -t cuda,nvtx,cublas,osrt ---gpu-metrics-devices=all --gpu-metrics-devices=gh100 -force-overwrite true -o profiling/$1 ./test_transformer_chunked_fwd
+nsys profile -t cuda,nvtx,cublas,osrt --gpu-metrics-devices=all --gpu-metrics-devices=gh100 -force-overwrite true -o profiling/$1 ./test_transformer_chunked_fwd
