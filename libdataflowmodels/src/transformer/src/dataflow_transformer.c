1f1cd2faa18b7cd37f56197066570861ce777973
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2025-05-12 18:44:10 -0400

    Making default be not printing/saving data...

--- libdataflowmodels/src/transformer/src/dataflow_transformer.c
+++ libdataflowmodels/src/transformer/src/dataflow_transformer.c
@@ -2,11 +2,11 @@
 #include "dataflow_transformer.h"
 
 // toggle to print out before submitting any ops...
-#define TO_PRINT 1
+#define TO_PRINT 0
 
 // meta-toggle required to be set to 1 to save any data
 // when set to 0, nothing will be saved
-#define TO_SAVE_DATA 1
+#define TO_SAVE_DATA 0
 
 
 // DETERMINES WHAT DATA TO SAVE...
