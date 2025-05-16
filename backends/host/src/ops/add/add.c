35ae0a63617524eb7e1520f42ca66731f1838341
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2025-05-16 00:01:06 -0400

    Updating host ops to account for grad accumulation and resetting to zero...

--- backends/host/src/ops/add/add.c
+++ backends/host/src/ops/add/add.c
@@ -3,6 +3,6 @@
 int do_add_host(DataflowDatatype A_dt, DataflowDatatype B_dt, DataflowDatatype C_dt,
                 int num_threads, size_t num_els, void * A, void * B, void * C){
     
-    printf(stderr, "Error: add_host not implemented...\n");
+    fprintf(stderr, "Error: add_host not implemented...\n");
     return -1;
 }
--- backends/host/src/ops/add/add_avx512.c
+++ backends/host/src/ops/add/add_avx512.c
@@ -3,6 +3,6 @@
 int do_add_host_avx512(DataflowDatatype A_dt, DataflowDatatype B_dt, DataflowDatatype C_dt,
                 int num_threads, size_t num_els, void * A, void * B, void * C){
     
-    printf(stderr, "Error: add_avx512 not implemented...\n");
+    fprintf(stderr, "Error: add_avx512 not implemented...\n");
     return -1;
 }
