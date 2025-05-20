c4a432a13290d0d8797eab1b67719d52451ae519
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2025-05-19 21:23:46 -0400

    Having option to print loss by step (avg across all chunks' avg)

---
+++ backends/host/src/ops/loss/print_loss.c
@@ -0,0 +1,46 @@
+#include "host_ops.h"
+
+
+int print_chunk_loss_host(void * _print_chunk_loss_host_op_args){
+    
+    Print_Chunk_Loss_Host_Op_Args * args = (Print_Chunk_Loss_Host_Op_Args *) _print_chunk_loss_host_op_args;
+
+    int step_num = args -> step_num;
+    int seq_id = args -> seq_id;
+    int chunk_id = args -> chunk_id;
+    int num_tokens = args -> num_tokens;
+    float * avg_loss_ref = args -> avg_loss_ref;
+
+    float avg_loss = *avg_loss_ref;
+
+    printf("[LOSS: Step %d, Seq %d, Chunk %d, Num Tokens: %d] Avg Loss: %f\n", step_num, seq_id, chunk_id, num_tokens, avg_loss);
+
+    return 0;
+}
+
+int print_step_loss_host(void * _print_step_loss_host_op_args){
+
+    Print_Step_Loss_Host_Op_Args * args = (Print_Step_Loss_Host_Op_Args *) _print_step_loss_host_op_args;
+
+    int step_num = args -> step_num;
+    int num_chunks = args -> num_chunks;
+    int total_tokens = args -> total_tokens;
+    float * per_chunk_avg_loss = args -> per_chunk_avg_loss;
+
+    float total_avg_chunk_loss = 0.0f;
+
+    for (int i = 0; i < num_chunks; i++){
+        total_avg_chunk_loss += per_chunk_avg_loss[i];
+    }
+
+    float avg_chunk_loss = total_avg_chunk_loss / num_chunks;
+
+    printf("[LOSS: Step %d, Num Chunks: %d, Total Tokens: %d] Avg Loss: %f\n", step_num, num_chunks, total_tokens, avg_chunk_loss);
+
+    return 0;
+}
+
+
+
+
+
