117301faf3995a68e3359a48bc9126443725b2e8
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2025-07-02 02:10:09 -0700

    Exposing function to compute training flops of seq...

--- backends/host/include/host_ops.h
+++ backends/host/include/host_ops.h
@@ -13,6 +13,10 @@
 int print_chunk_loss_host(void * _print_chunk_loss_host_op_args);
 int print_round_loss_host(void * _print_round_loss_host_op_args);
 
+float get_seq_flops(int seq_len, int vocab_size, int model_dim, int kv_dim, int num_shared_experts, int num_total_routed_experts, int num_active_routed_experts, int expert_dim, int num_layers, 
+							float * ret_seq_flops_fwd, float * ret_seq_flops_head, float * ret_seq_flops_bwd_x, float * ret_seq_flops_bwd_w);
+
+
 int start_step_metrics(void * _step_throughput_op_args);
 int end_step_metrics(void * _step_throughput_op_args);
 
