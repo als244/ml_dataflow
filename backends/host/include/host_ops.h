3ff0cc3943f9ef6a2cf23080fe03f504986e4bb0
als244 <andrew.sheinberg@yale.edu>
2025-07-07 11:18:52 -0700

    Correcting attn flop calculation for bwd pass...

--- backends/host/include/host_ops.h
+++ backends/host/include/host_ops.h
@@ -13,7 +13,7 @@
 int print_chunk_loss_host(void * _print_chunk_loss_host_op_args);
 int print_round_loss_host(void * _print_round_loss_host_op_args);
 
-float get_seq_flops(int seq_len, int vocab_size, int model_dim, int kv_dim, int num_shared_experts, int num_total_routed_experts, int num_active_routed_experts, int expert_dim, int num_layers, 
+float get_seq_flops(int seq_len, int vocab_size, int model_dim, int kv_dim, int is_causal, int num_shared_experts, int num_total_routed_experts, int num_active_routed_experts, int expert_dim, int num_layers, 
 							float * ret_seq_flops_fwd, float * ret_seq_flops_head, float * ret_seq_flops_bwd_x, float * ret_seq_flops_bwd_w, float * ret_seq_attn_flops, float * ret_seq_matmul_flops);
 
 int start_step_metrics(void * _step_throughput_op_args);
