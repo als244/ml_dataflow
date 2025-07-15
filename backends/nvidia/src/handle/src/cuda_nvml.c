264066546a030d3ccd59fed1aec305be6c728488
als244 <andrew.sheinberg@yale.edu>
2025-07-15 11:17:21 -0700

    Detecting PCIe info upon init

--- backends/nvidia/src/handle/src/cuda_nvml.c
+++ backends/nvidia/src/handle/src/cuda_nvml.c
@@ -21,14 +21,14 @@ int cuda_nvml_get_pcie_info(int device_id, unsigned int * pcie_link_width, unsig
     }
 
     // Get PCIe link width
-    result = nvmlDeviceGetPcieLinkWidth(device, pcie_link_width);
+    result = nvmlDeviceGetCurrPcieLinkWidth(device, pcie_link_width);
     if (NVML_SUCCESS != result) {
         fprintf(stderr, "Failed to get PCIe link width for device %d: %s\n", device_id, nvmlErrorString(result));
         return -1;
     }
 
     // Get PCIe link speed (generation)
-    result = nvmlDeviceGetPcieLinkGen(device, pcie_link_gen);
+    result = nvmlDeviceGetCurrPcieLinkGen(device, pcie_link_gen);
     if (NVML_SUCCESS != result) {
         fprintf(stderr, "Failed to get PCIe link speed for device %d: %s\n", device_id, nvmlErrorString(result));
         return -1;
