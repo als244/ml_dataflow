73cc39b7c78bf37b2eff55b93ab34b3fa50b1bea
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2025-08-02 02:00:58 -0700

    Comparing MoE correctness...

--- test/dataflow_models/correctness_compare/test_moe/train_moe_flash2.py
+++ test/dataflow_models/correctness_compare/test_moe/train_moe_flash2.py
@@ -21,62 +21,21 @@ TO_SAVE_PREDS = False
 TO_SAVE_OPT_STATE = False
 TO_SAVE_PARAM_GRADS = False
 
+MODEL_PATH = "./full_model.pth"
 
+TRAIN_SEQ_LEN = 8192
 
-SEED = 0
-
-## python convert_llama_weights_to_hf.py --input_dir /mnt/storage/models/llama3/meta_checkpoints/8B_inst --model_size 8B --output_dir . --llama_version 3.1
-
-torch.manual_seed(SEED)
-np.random.seed(SEED)
-random.seed(SEED)
-
-MAX_SEQ_LEN = 8192
-MAX_BATCH_SIZE = 1
-
-MODEL_PATH = "./models/1B_inst/"
-
-with open(MODEL_PATH + "params.json", "r") as f:
-	model_args = json.loads(f.read())
-
-params = ModelArgs(max_seq_len=MAX_SEQ_LEN, max_batch_size=MAX_BATCH_SIZE, **model_args)
-
-llama_tokenizer = Tokenizer(MODEL_PATH + "tokenizer.model")
-
-checkpoint_paths = sorted(glob.glob(MODEL_PATH + "*.pth"))
-checkpoints = [torch.load(x, map_location="cpu") for x in checkpoint_paths]
-
-assert params.vocab_size == llama_tokenizer.n_words
-
-torch.set_default_tensor_type(torch.cuda.BFloat16Tensor)
-
-print(f"Initializing Model (on host)...\n\tVocab Size: {params.vocab_size}\n\tModel Dim: {params.dim}\n\t# Layers: {params.n_layers}\n\t# Heads: {params.n_heads}\n\t# KV Heads: {params.n_kv_heads}\n\tMax Batch Size: {params.max_batch_size}\n\tMax Seq Len: {params.max_seq_len}\n\n\n")
-
-start = time.time_ns()
-
-model = Transformer(params)
-
-for c in checkpoints:
-	for name, tensor in c.items():
-		c[name] = tensor
-	model.load_state_dict(c, strict=False)
-
-stop = time.time_ns()
-
-time_ms = (stop - start) / 1e6
-
-print(f"Finished Initialized Model!\n\tRuntime: {time_ms} ms\n")
-
+model = torch.load(MODEL_PATH)
 
 device = torch.device("cuda:0")
 
 token_id_file = "65536_token_ids_uint32.dat"
 token_labels_file = "65536_labels_uint32.dat"
-np_inp_tokens = np.fromfile(token_id_file, dtype=np.uint32)[:MAX_SEQ_LEN]
-np_labels = np.fromfile(token_labels_file, dtype=np.uint32)[:MAX_SEQ_LEN]
+np_inp_tokens = np.fromfile(token_id_file, dtype=np.uint32)[:TRAIN_SEQ_LEN]
+np_labels = np.fromfile(token_labels_file, dtype=np.uint32)[:TRAIN_SEQ_LEN]
 
-seqlens_q_np = np.array([MAX_SEQ_LEN], dtype=np.int32)
-seqlens_k_np = np.array([MAX_SEQ_LEN], dtype=np.int32)
+seqlens_q_np = np.array([TRAIN_SEQ_LEN], dtype=np.int32)
+seqlens_k_np = np.array([TRAIN_SEQ_LEN], dtype=np.int32)
 
 device = torch.device("cuda:0")
 
