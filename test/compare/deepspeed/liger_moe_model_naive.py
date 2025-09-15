da30aeafe90999ebe0b11ca08fad07808086f44f
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2025-09-14 23:33:38 -0700

    Updated moe impl

--- test/compare/deepspeed/liger_moe_model_naive.py
+++ test/compare/deepspeed/liger_moe_model_naive.py
@@ -111,37 +111,75 @@ class Attention(nn.Module):
         
         nvtx.range_pop() # NVTX End
         return result
+    
+class ExpertMLP(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.hidden_size = args.dim
+        self.expert_dim = args.expert_dim
+        self.gate_proj = nn.Linear(self.hidden_size, self.expert_dim, bias=False)
+        self.up_proj = nn.Linear(self.hidden_size, self.expert_dim, bias=False)
+        self.down_proj = nn.Linear(self.expert_dim, self.hidden_size, bias=False)
+
+    def forward(self, x):
+        down_proj = self.down_proj(F.silu(self.gate_proj(x)) * self.up_proj(x))
+        return down_proj
+
 
 class MoEFeedForward(nn.Module):
     def __init__(self, args: ModelArgs, layer_id):
         super().__init__()
          
-        self.has_saved = False
+     
         self.layer_id = layer_id
         self.top_k = args.top_k
-        self.router = nn.Linear(args.dim, args.num_experts, bias=False, dtype=args.dtype)
-        self.moe_layer = GLUMLP(input_size=args.dim, hidden_size=args.expert_dim, activation=nn.SiLU(), num_experts=args.num_experts, top_k=args.top_k)
+        self.gate = nn.Linear(args.dim, args.num_experts, bias=False, dtype=args.dtype)
+        self.experts = nn.ModuleList(
+            [ExpertMLP(args) for _ in range(args.num_experts)]
+        )
+        
         
 
     def forward(self, x: torch.Tensor):
         nvtx.range_push("MoE") # NVTX Start
 
-        batch_size, sequence_length, hidden_dim = x.shape
+        batch_size, sequence_length, hidden_dim = hidden_states.shape
+        hidden_states = hidden_states.view(-1, hidden_dim)
+        # router_logits: (batch * sequence_length, n_experts)
+        router_logits = self.gate(hidden_states)
 
-        x = x.view(-1, hidden_dim)
-        routed_x = self.router(x)
-
-        routing_weights = F.softmax(routed_x, dim=1, dtype=torch.float32)
+        routing_weights = F.softmax(router_logits, dim=1, dtype=torch.float)
         routing_weights, selected_experts = torch.topk(routing_weights, self.top_k, dim=-1)
+        if self.norm_topk_prob:  # only diff with mixtral sparse moe block!
+            routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
+        # we cast back to the input dtype
+        routing_weights = routing_weights.to(hidden_states.dtype)
 
-        routing_weights /= routing_weights.sum(dim=-1, keepdim=True)
+        final_hidden_states = torch.zeros(
+            (batch_size * sequence_length, hidden_dim), dtype=hidden_states.dtype, device=hidden_states.device
+        )
+
+        # One hot encode the selected experts to create an expert mask
+        # this will be used to easily index which expert is going to be sollicitated
+        expert_mask = torch.nn.functional.one_hot(selected_experts, num_classes=self.num_experts).permute(2, 1, 0)
+
+        # Loop over all available experts in the model and perform the computation on each expert
+        expert_hit = torch.greater(expert_mask.sum(dim=(-1, -2)), 0).nonzero()
+        for expert_idx in expert_hit:
+            expert_layer = self.experts[expert_idx]
+            idx, top_x = torch.where(expert_mask[expert_idx].squeeze(0))
+
+            # Index the correct hidden states and compute the expert hidden state for
+            # the current expert. We need to make sure to multiply the output hidden
+            # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
+            current_state = hidden_states[None, top_x].reshape(-1, hidden_dim)
+            current_hidden_states = expert_layer(current_state) * routing_weights[top_x, idx, None]
+
+            # However `index_add_` only support torch tensors for indexing so we'll use
+            # the `top_x` tensor here.
+            final_hidden_states.index_add_(0, top_x, current_hidden_states.to(hidden_states.dtype))
+        final_hidden_states = final_hidden_states.reshape(batch_size, sequence_length, hidden_dim)
         
-        routing_weights = routing_weights.to(x.dtype)
-        
-        final_hidden_states = self.moe_layer(x, routing_weights, selected_experts)
-
-        final_hidden_states = final_hidden_states.view(batch_size, sequence_length, hidden_dim)
-
         nvtx.range_pop() # NVTX End
         return final_hidden_states
 
