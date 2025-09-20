a249b25790bdd61b935251a3093eae87a165a630
als244 <andrew.sheinberg@gmail.com>
2025-09-20 11:38:27 -0400

    Update dense training

--- test/compare/deepspeed/liger_train.py
+++ test/compare/deepspeed/liger_train.py
@@ -37,34 +37,33 @@ model_args = ModelArgs(
 
 epochs = 1
 learning_rate = 1e-5
-grad_accum_steps = 38
+grad_accum_steps = 19
 total_steps = 3 * grad_accum_steps
 
 # --- DeepSpeed Configuration with Logging ---
 # The ds_config can be loaded from a JSON file specified by --deepspeed_config
 # For simplicity, we define it here.
 ds_config = {
-    "train_micro_batch_size_per_gpu": 2,
+    "train_micro_batch_size_per_gpu": 4,
     "gradient_accumulation_steps": grad_accum_steps,
     "wall_clock_breakdown": True,
     "optimizer": { "type": "AdamW", "params": { "lr": learning_rate, "betas": [0.9, 0.95], "fp32_optimizer_states": False} },
     "bf16": { "enabled": True},
-    "gradient_clipping": 1.0,
     "steps_per_print": 1,
-    "activation_checkpointing": { "partition_activations": True, "cpu_checkpointing": True },
+    "activation_checkpointing": { "cpu_checkpointing": True },
 
     # === ADDED: A safe baseline configuration for initialization ===
     # This will be used to successfully load the model before tuning begins.
     "zero_optimization": {
-        "stage": 3,
+        "stage": 1,
         "offload_optimizer": {
             "device": "cpu",
             "pin_memory": True
         },
-        "offload_param": {
-            "device": "cpu",
-            "pin_memory": True
-        },
+        #"offload_param": {
+        #    "device": "cpu",
+        #    "pin_memory": True
+        #},
     },
 
     
@@ -132,6 +131,8 @@ model_engine, optimizer, training_dataloader, _ = deepspeed.initialize(
 )
 
 
+torch.cuda.empty_cache()
+
 # --- Training Loop with Throughput Calculation ---
 print(f"Starting training with sequence length: {args.seq_len}...")
 
@@ -167,12 +168,17 @@ for epoch in range(epochs):
                 mem_alloc = torch.cuda.memory_allocated() / 1024**3
                 print(
                     f"Epoch: {epoch+1}, Step: {i+1}, BS: {actual_bs}, Loss: {loss.item():.4f} | "
-                    f"Total_tokens: {total_tokens}, Total_time = {elapsed_time}, Tok/sec: {tokens_per_sec:.2f} | VRAM: {mem_alloc:.2f}GB"
+                    f"Step total_tokens: {total_tokens}, Step total_time = {elapsed_time}, Tok/sec: {tokens_per_sec:.2f} | VRAM: {mem_alloc:.2f}GB"
                 )
+                start_time = time.time()
+                total_tokens = 0
+
 
         if num_steps == total_steps:
             break
 
 ret = _cudart.cudaProfilerStop()
 
-print("\nTraining complete! ✅")
+peak_mem_reserved_gb = torch.cuda.max_memory_reserved() / 1024**3
+
+print(f"\nTraining complete! ✅\n\tPeak Memory Reserved: {peak_mem_reserved_gb:.2f} GB")
